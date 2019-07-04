83c1709ed5a820c9efd4a6bdeb42c837cdd86eca
Khoa Nguyen Dang Tran <khoandtran@ilavietnam.com>
2019-07-04 16:40:15 +0700

    update 04-07-2019

--- js/script.js
+++ js/script.js
@@ -1,12 +1,29 @@
-var swiper = new Swiper('.swiper-container', {
+var swiper = new Swiper('.slider-main .swiper-container', {
     loop: true,
     slidesPerView: 'auto',
     spaceBetween: 30,
     centeredSlides: true,
-//    autoplay: {
-//        delay: 5000,
-//        disableOnInteraction: false,
-//    },
+    autoplay: {
+        delay: 5000,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+});
+
+var swiper = new Swiper('.doubleSlider .swiper-container', {
+    loop: true,
+    slidesPerView: 2,
+    slidesPerGroup: 2,
+    spaceBetween: 20,
+    loopFillGroupWithBlank: true,
+    centeredSlides: false,
+    autoplay: {
+        delay: 6000,
+        disableOnInteraction: false,
+    },
     pagination: {
         el: '.swiper-pagination',
         clickable: true,

75ede2a8cbd9b9306c10485686afa291be976aaf
Tay Ninh <tndkhoa1104@yahoo.com>
2019-07-13 09:49:43 +0700

    update responsive TN

--- js/script.js
+++ js/script.js
@@ -11,6 +11,24 @@ var swiper = new Swiper('.slider-main .swiper-container', {
         el: '.swiper-pagination',
         clickable: true,
     },
+    breakpoints: {
+        320: {
+            slidesPerView: 1,
+            spaceBetween: 10
+        },
+        480: {
+            slidesPerView: 2,
+            spaceBetween: 20
+        },
+        640: {
+            slidesPerView: 3,
+            spaceBetween: 30
+        }
+        ,
+        1200: {
+            spaceBetween: 20
+        }
+    }
 });
 
 var swiper = new Swiper('.doubleSlider .swiper-container', {
@@ -28,6 +46,25 @@ var swiper = new Swiper('.doubleSlider .swiper-container', {
         el: '.swiper-pagination',
         clickable: true,
     },
+    breakpoints: {
+        320: {
+            slidesPerView: 1,
+            spaceBetween: 10
+        },
+        480: {
+            slidesPerView: 2,
+            spaceBetween: 20
+        },
+        640: {
+            slidesPerView: 3,
+            spaceBetween: 30
+        }
+        ,
+        1200: {
+            slidesPerView: 1,
+            slidesPerGroup: 1,
+        }
+    }
 });
 
 wow = new WOW({
